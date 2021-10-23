8788b1286a7ebd3ac156824a5d15595d4a266f06
EVOKE <evoke.lektrique@gmail.com>
2021-10-23 10:58:19 +0330

    Add draw functionality

--- core.js
+++ core.js
@@ -56,22 +56,65 @@ function calculate_obstacles(ball_volume, vehicle_volume) {
    return total_balls_to_fit;
 }
 
-// Create a vehicle object and setup its properties in inches,
-// And I know these numbers don't make any sense but yeah.
-const v  = Vehicle;
-v.name   = "car";
-v.width  = 100;
-v.height = 50;
-v.length = 1000;
+/**
+ * Convert meter to inch
+ *
+ * @param  {Integer} meter
+ * @return {Integer}       Inch
+ */
+const to_inch = (meter) => {
+   return meter * 39.37;
+}
 
-// Calcualte ball volume
-const diameter    = 2;
-const ball_volume = Ball.calculate(diameter);
+/**
+ * Get element by ID
+ *
+ * @param  {String} id Element ID
+ * @return {Object}    Element
+ */
+function get(id) {
+   return document.getElementById(id);
+}
 
-// Calculate vehicle volume
-const vehicle_volume = v.calculate();
+/**
+ * Calculate
+ */
+function draw() {
+   const ball_diameter  = to_inch(parseFloat(get('b-diameter').value) || 0);
+   const vehicle_name   = get('v-name').value;
+   const vehicle_width  = to_inch(parseFloat(get('v-width').value) || 0);
+   const vehicle_height = to_inch(parseFloat(get('v-height').value) || 0);
+   const vehicle_length = to_inch(parseFloat(get('v-length').value) || 0);
+   const result         = get('result');
 
-// Calcualte how many balls can fit in a vehicle
-const total_balls_to_fit = calculate_obstacles(ball_volume, vehicle_volume);
+   // Create a vehicle object and setup its properties in inches,
+   // And I know these numbers don't make any sense but yeah.
+   const v  = Vehicle;
+   v.name   = vehicle_name;
+   v.width  = vehicle_width;
+   v.height = vehicle_height;
+   v.length = vehicle_length;
 
-console.log(`We can fit ${total_balls_to_fit} balls in a ${v.name}`);
+   // Calcualte ball volume
+   const ball_volume = Ball.calculate(ball_diameter);
+
+   // Calculate vehicle volume
+   const vehicle_volume = v.calculate();
+
+   // Calcualte how many balls can fit in a vehicle
+   const total_balls_to_fit = Math.floor(calculate_obstacles(ball_volume, vehicle_volume));
+
+   const message = `You can fit <span class='green'>${total_balls_to_fit}</span> balls in a <span class='green'>${v.name}</span>`;
+   result.innerHTML = message;
+}
+
+// Add an event listener to each input so whenever
+// they have been changed draw a new result in DOM.
+Array.from(document.querySelectorAll('input')).forEach(input => {
+   input.addEventListener('keyup', e => {
+      draw();
+   });
+});
+
+// Draw result at start
+draw();
